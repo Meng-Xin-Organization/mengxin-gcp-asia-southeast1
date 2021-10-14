ddc37d157d2ff343c4fae2ffc064995da20d8469
mengxin <mengxin891029@gmail.com>
2021-10-14 21:51:18 +0800

    update multi-architecture full

--- deploy.sh
+++ deploy.sh
@@ -17,24 +17,27 @@ kubectl set image --namespace web deployments/sea-mengxin-ml-deployment sea-meng
 
 
 # update chat-mengxin-ml
-docker build -t mengxin891029/chat-mengxin-ml:latest -t mengxin891029/chat-mengxin-ml:$GIT_SHA -f ./docker-images/chat/Dockerfile ./docker-images/chat
-docker push mengxin891029/chat-mengxin-ml:$GIT_SHA
-docker push mengxin891029/chat-mengxin-ml:latest
+# docker build -t mengxin891029/chat-mengxin-ml:latest -t mengxin891029/chat-mengxin-ml:$GIT_SHA -f ./docker-images/chat/Dockerfile ./docker-images/chat
+# docker push mengxin891029/chat-mengxin-ml:$GIT_SHA
+# docker push mengxin891029/chat-mengxin-ml:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/chat-mengxin-ml:latest -t mengxin891029/chat-mengxin-ml:$GIT_SHA -f ./docker-images/chat/Dockerfile ./docker-images/chat
 kubectl apply -f k8s/web/chat-mengxin-ml
 kubectl set image --namespace web deployments/chat-mengxin-ml-deployment chat-mengxin-ml=mengxin891029/chat-mengxin-ml:$GIT_SHA
 
 
 # update tunnel-mengxin-ml
-docker build -t mengxin891029/tunnel-mengxin-ml:latest -t mengxin891029/tunnel-mengxin-ml:$GIT_SHA -f ./docker-images/tunnel/Dockerfile ./docker-images/tunnel
-docker push mengxin891029/tunnel-mengxin-ml:$GIT_SHA
-docker push mengxin891029/tunnel-mengxin-ml:latest
+# docker build -t mengxin891029/tunnel-mengxin-ml:latest -t mengxin891029/tunnel-mengxin-ml:$GIT_SHA -f ./docker-images/tunnel/Dockerfile ./docker-images/tunnel
+# docker push mengxin891029/tunnel-mengxin-ml:$GIT_SHA
+# docker push mengxin891029/tunnel-mengxin-ml:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/tunnel-mengxin-ml:latest -t mengxin891029/tunnel-mengxin-ml:$GIT_SHA -f ./docker-images/tunnel/Dockerfile ./docker-images/tunnel
 kubectl apply -f k8s/web/tunnel-mengxin-ml
 kubectl set image --namespace web deployments/tunnel-mengxin-ml-deployment tunnel-mengxin-ml=mengxin891029/tunnel-mengxin-ml:$GIT_SHA
 
 # update grpc-mengxin-ml
-docker build -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
-docker push mengxin891029/grpc-mengxin-ml:$GIT_SHA
-docker push mengxin891029/grpc-mengxin-ml:latest
+# docker build -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
+# docker push mengxin891029/grpc-mengxin-ml:$GIT_SHA
+# docker push mengxin891029/grpc-mengxin-ml:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/grpc-mengxin-ml:latest -t mengxin891029/grpc-mengxin-ml:$GIT_SHA -f ./docker-images/grpc/Dockerfile ./docker-images/grpc
 kubectl apply -f k8s/web/grpc-mengxin-ml
 kubectl set image --namespace web deployments/grpc-mengxin-ml-deployment grpc-mengxin-ml=mengxin891029/grpc-mengxin-ml:$GIT_SHA
 
@@ -48,9 +51,10 @@ kubectl apply -f k8s/web/httpbin-mengxin-ml
 kubectl apply -f k8s/web/mongo-express-mengxin-ml
 
 # update jupyter-mengxin-ml
-docker build -t mengxin891029/jupyter-mengxin-ml:latest -t mengxin891029/jupyter-mengxin-ml:$GIT_SHA -f ./docker-images/jupyter/Dockerfile ./docker-images/jupyter
-docker push mengxin891029/jupyter-mengxin-ml:$GIT_SHA
-docker push mengxin891029/jupyter-mengxin-ml:latest
+# docker build -t mengxin891029/jupyter-mengxin-ml:latest -t mengxin891029/jupyter-mengxin-ml:$GIT_SHA -f ./docker-images/jupyter/Dockerfile ./docker-images/jupyter
+# docker push mengxin891029/jupyter-mengxin-ml:$GIT_SHA
+# docker push mengxin891029/jupyter-mengxin-ml:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/jupyter-mengxin-ml:latest -t mengxin891029/jupyter-mengxin-ml:$GIT_SHA -f ./docker-images/jupyter/Dockerfile ./docker-images/jupyter
 kubectl apply -f k8s/web/jupyter-mengxin-ml
 kubectl set image --namespace web deployments/jupyter-mengxin-ml-deployment jupyter-mengxin-ml=mengxin891029/jupyter-mengxin-ml:$GIT_SHA
 
@@ -72,9 +76,10 @@ kubectl apply -f k8s/web
 
 
 # update ssl-mengxin-ml-keyless
-docker build -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
-docker push mengxin891029/keyless:$GIT_SHA
-docker push mengxin891029/keyless:latest
+# docker build -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
+# docker push mengxin891029/keyless:$GIT_SHA
+# docker push mengxin891029/keyless:latest
+docker buildx build --platform linux/amd64,linux/arm64 --push -t mengxin891029/keyless:latest -t mengxin891029/keyless:$GIT_SHA -f ./docker-images/keyless/Dockerfile ./docker-images/keyless
 kubectl apply -f k8s/tcp/ssl-mengxin-ml-keyless
 kubectl set image --namespace tcp deployments/ssl-mengxin-ml-keyless-deployment ssl-mengxin-ml=mengxin891029/keyless:$GIT_SHA
 
